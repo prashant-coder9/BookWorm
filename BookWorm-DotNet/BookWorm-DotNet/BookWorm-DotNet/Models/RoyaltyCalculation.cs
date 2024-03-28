4f94551f70cb1708bc58aa08c0bc7f38394b5ef8
Prashant Vyavhare <128772161+prashant-coder9@users.noreply.github.com>
2024-03-28 15:51:05 +0530

    Add files via upload

---
+++ BookWorm-DotNet/BookWorm-DotNet/BookWorm-DotNet/Models/RoyaltyCalculation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BookWorm_DotNet.Models;
+
+public partial class RoyaltyCalculation
+{
+    public long RoyaltyCalculationId { get; set; }
+
+    public double? BasePrice { get; set; }
+
+    public long? BeneficiaryId { get; set; }
+
+    public long? ProductId { get; set; }
+
+    public DateTime? RoyaltyCalculationDate { get; set; }
+
+    public double? RoyaltyOnBasePrice { get; set; }
+
+    public double? SalePrice { get; set; }
+
+    public string? TransactionType { get; set; }
+}
